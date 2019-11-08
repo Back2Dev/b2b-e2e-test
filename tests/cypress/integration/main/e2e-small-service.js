2ad94da7763cf72476142bacff2e745f6e3dd2a7
Patrick Carmel <pato@live.com.au>
2019-11-08 19:55:31 +1100

    entered data on second webpage of assessment upto third

--- tests/cypress/integration/main/e2e-small-service.js
+++ tests/cypress/integration/main/e2e-small-service.js
@@ -23,30 +23,72 @@ describe('Small Service Test', function() {
     cy.get('input#root_assessor')
       .click()
       .type('Pat Caramello')
-
+    cy.get('#root_bikeMake')
+      .clear()
+      .type('Shimano')
     cy.get('input#root_bikeModel')
       .clear()
       .type('Very Fast')
 
     cy.get('#root_bikeColor').type('Red')
 
-    cy.get('#approxBikeValue').type('$200.00')
-    // cy.get('#root_serivces_0').uncheck()
-    // //   .uncheck(value)
-    // cy.get('#root_serivces_1').uncheck()
-    // //   .uncheck(value)
-    // cy.get('#root_serivces_2').uncheck()
-    // // .uncheck(value)
-    // cy.get('#root_serivces_3').uncheck()
-    // //.uncheck(value)
-    // cy.get('#root_serivces_4').uncheck()
-    // //.uncheck(value)
-    // cy.get('#root_serivces_5').uncheck()
-    // //.uncheck(value)
-    // cy.get('#root_serivces_6').uncheck()
-    // //.uncheck(value)
+    cy.get('#root_approxBikeValue')
+      .clear()
+      .type('200')
+    cy.get('#root_services_0').uncheck()
 
-    // cy.get('#root_serivces_7').check()
-    // cy.get('#root_serivces_9').check()
+    cy.get('#root_services_1').uncheck()
+
+    cy.get('#root_services_2').uncheck()
+
+    cy.get('#root_services_3').uncheck()
+
+    cy.get('#root_services_7').check()
+    cy.get('#root_services_9').check()
+  })
+  it('clicks the next button, "type"', function() {
+    cy.get('button')
+      .contains('Next')
+      .click()
+  })
+  it('Select parts and add further work, "type"', function() {
+    cy.get('#root_parts_9').check() // checks the rear brake cable option.
+    cy.get('#root_parts_5').check() // checks the V brake pad option.
+    cy.get('#root_parts_13').check()
+
+    cy.get('#root_comments')
+      .clear()
+      .click()
+      .type('Had to wait for more parts to come in.')
+
+    cy.get('#root_additionalFee')
+      .clear()
+      .type('50')
+
+    cy.get('#root_discount')
+      .clear()
+      .type('10')
+
+    cy.get('#root_discountReason').type('late with delivery')
+  })
+
+  it('Click buttton to go to next page, "type"', function() {
+    cy.get('button')
+      .contains('Next')
+      .click()
+  })
+
+  it('Click buttton to go to next page, "type"', function() {
+    cy.get('button')
+      .contains('Next')
+      .click()
+  })
+
+  it('Enters customer data in the form, "type"', function() {
+    cy.get('#root_pickUpDate').type('2019-11-12')
+
+    cy.get('ui input')
+      .contains('Name')
+      .type('Patrick Carmel')
   })
 })
